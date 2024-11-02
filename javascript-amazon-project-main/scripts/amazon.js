27f0e33365cc0f00b7acf18a5774bf9a192046b6
switjeph <aladinojephthah@gmail.com>
2024-11-02 11:16:39 +0100

    make the cart quantity intraractive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -80,6 +80,12 @@ button.addEventListener('click', () => {
     });
    }
 
-    console.log(cart);  
+   let cartQuantity = 0; 
+
+cart.forEach((item) => {
+    cartQuantity += item.quantity;
+});
+
+document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 });
 });
