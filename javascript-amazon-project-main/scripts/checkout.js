4a360ffe87a0cd0ce8b7924dd1ff66aefb5e32b4
switjeph <aladinojephthah@gmail.com>
2024-11-05 08:57:35 +0100

    delete products from html when clicking delete

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -17,7 +17,8 @@ cart.forEach((cartItem) => {
     });
     
    cartSummaryHTML += `
-<div class="cart-item-container">
+<div class="cart-item-container 
+    js-cart-item-container-${matchingProcuct.id}">
 <div class="delivery-date">
     Delivery date: Tuesday, June 21
 </div>
@@ -102,5 +103,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
        const productId = link.dataset.productId;
        removeFromCart(productId);
 
+       const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+       );
+       container.remove();
+       
     });
 });
